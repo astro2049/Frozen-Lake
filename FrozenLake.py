49d6db30448ad13147b633c975ed63fa42d2a9ac
adarsh046 <gadarsh046@gmail.com>
2023-01-04 14:48:57 +0000

    Update FrozenLake.py

--- FrozenLake.py
+++ FrozenLake.py
@@ -358,7 +358,7 @@ def sarsa(env, max_episodes, eta, gamma, epsilon, seed=None):
     random_state = np.random.RandomState(seed)
  
     eta = np.linspace(eta, 0, max_episodes)
-    #epsilon = np.linspace(epsilon, 0, max_episodes)
+    epsilon = np.linspace(epsilon, 0, max_episodes)
  
     q = np.zeros((env.n_states, env.n_actions))
  
